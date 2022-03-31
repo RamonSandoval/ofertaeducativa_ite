7da991c42d48d307a89eacc0ce0a0e8385fd5aa8
Aylin08 <75355673+Aylin08@users.noreply.github.com>
2022-03-31 16:33:20 -0700

    Revert "Merge branch 'main' of https://github.com/RamonSandoval/ofertaeducativa_ite"
    This reverts commit 8f60fe02a342c11f6b4b818d7f8bd4cd1743b981, reversing
    changes made to 5913fa59948d0cee6067e76577a709fd245fb375.

---
+++ lib/pages/licenciaturas/ing_electronica.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ingElectronica extends StatefulWidget {
+  const ingElectronica({Key? key}) : super(key: key);
+
+  @override
+  State<ingElectronica> createState() => _ingElectronicaState();
+}
+
+class _ingElectronicaState extends State<ingElectronica> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/pages/licenciaturas/ing_sistemas.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ingSistemas extends StatefulWidget {
+  const ingSistemas({Key? key}) : super(key: key);
+
+  @override
+  State<ingSistemas> createState() => _ingSistemasState();
+}
+
+class _ingSistemasState extends State<ingSistemas> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
