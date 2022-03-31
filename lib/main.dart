7127385b0d768beffa2812c7abc47a57c915393d
RamonSandoval <102700747+RamonSandoval@users.noreply.github.com>
2022-03-31 16:44:19 -0700

    Cambio en todo
    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:ofertaeducativa_ite/pages/HomePage.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Aysnc',
+      home: MyHomePage(),
     );
   }
 }
-
